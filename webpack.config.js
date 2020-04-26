cb027254bb8ffa0bb920afd84c025198d568f000
tostos <saittosun2@gmail.com>
2020-04-26 13:29:52 +0200

    loading css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // This is node.js syntax because a webpack runs on Node.js and in the end it just exploits this object from this file and this has to be our configuration object.
 // jshint esversion: 6
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: 'development',
@@ -25,6 +26,26 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            }
+          },
+          {loader: 'postcss-loader', options: {
+            ident: 'postcss',
+            plugins: () => [autoprefixer()]
+          }}
+        ]
       }
     ]
   }
